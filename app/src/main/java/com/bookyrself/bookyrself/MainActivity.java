beff3a034fd9afa167e69d399b5efbb0f035fe22
manbradcalf <ben.medcalf@gmail.com>
2017-08-11 14:47:31 -0400

    first commit - yolo. don't feel like messing with gitignore right now

---
+++ app/src/main/java/com/bookyrself/bookyrself/MainActivity.java
@@ -0,0 +1,77 @@
+package com.bookyrself.bookyrself;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public abstract class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    protected BottomNavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getContentViewId());
+
+        navigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    // Remove inter-activity transition to avoid screen tossing on tapping bottom navigation items
+    @Override
+    public void onPause() {
+        super.onPause();
+        overridePendingTransition(0, 0);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        updateNavigationBarState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        navigationView.postDelayed(() -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.navigation_search) {
+                startActivity(new Intent(this, SearchActivity.class));
+            } else if (itemId == R.id.navigation_messages) {
+                startActivity(new Intent(this, MessagesActivity.class));
+            } else if (itemId == R.id.navigation_calendar) {
+                startActivity(new Intent(this, CalendarActivity.class));
+            } else if (itemId == R.id.navigation_profile) {
+                startActivity(new Intent(this, ProfileActivity.class));
+            }
+            finish();
+        }, 300);
+    return true;
+    }
+
+    private void updateNavigationBarState(){
+        int actionId = getNavigationMenuItemId();
+        selectBottomNavigationBarItem(actionId);
+    }
+
+    void selectBottomNavigationBarItem(int itemId) {
+        Menu menu = navigationView.getMenu();
+        for (int i = 0, size = menu.size(); i < size; i++) {
+            MenuItem item = menu.getItem(i);
+            boolean shouldBeChecked = item.getItemId() == itemId;
+            if (shouldBeChecked) {
+                item.setChecked(true);
+                break;
+            }
+        }
+    }
+
+    abstract int getContentViewId();
+
+    abstract int getNavigationMenuItemId();
+
+}
---
+++ app/src/main/java/com/bookyrself/bookyrself/ProfileActivity.java
@@ -0,0 +1,34 @@
+package com.bookyrself.bookyrself;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ProfileActivity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(view -> Snackbar.make(view, "ProfileActivity", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+    }
+
+    @Override
+    int getContentViewId() {
+        return R.layout.activity_profile;
+    }
+
+    @Override
+    int getNavigationMenuItemId() {
+        return R.id.navigation_profile;
+    }
+
+}
