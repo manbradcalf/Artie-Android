b7d6daf82fd18a678788f797f3e768cd41cfa4d0
manbradcalf <ben.medcalf@gmail.com>
2018-03-11 13:30:46 -0400

    adding some drawables

---
+++ app/src/main/java/com/bookyrself/bookyrself/presenters/EventCreationPresenter.java
@@ -0,0 +1,8 @@
+package com.bookyrself.bookyrself.presenters;
+
+/**
+ * Created by benmedcalf on 3/9/18.
+ */
+
+public class EventCreationPresenter {
+}
---
+++ app/src/main/java/com/bookyrself/bookyrself/utils/EventDecorator.java
@@ -0,0 +1,8 @@
+package com.bookyrself.bookyrself.utils;
+
+/**
+ * Created by benmedcalf on 3/10/18.
+ */
+
+public class EventDecorator {
+}
