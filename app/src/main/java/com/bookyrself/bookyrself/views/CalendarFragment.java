f36f73bfc13e472ed5587e51fb9b2d54aae79a8a
manbradcalf <ben.medcalf@gmail.com>
2018-04-08 13:16:26 -0400

    starting eventdetailactivity when clicking on a date on the calendarfragment

--- app/src/main/java/com/bookyrself/bookyrself/views/CalendarFragment.java
+++ app/src/main/java/com/bookyrself/bookyrself/views/CalendarFragment.java
@@ -1,13 +1,16 @@
 package com.bookyrself.bookyrself.views;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.bookyrself.bookyrself.R;
 import com.bookyrself.bookyrself.models.SearchResponseUsers.Event;
@@ -18,11 +21,14 @@ import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class CalendarFragment extends Fragment implements OnDateSelectedListener, CalendarPresenter.CalendarPresenterListener {
     private MaterialCalendarView calendarView;
     private CalendarPresenter presenter;
+    List<CalendarDay> calendarDays = new ArrayList<>();
+    HashMap<CalendarDay, String> calendarDaysWithEventIds;
 
     @Override
     public void onCreate(Bundle savedInsanceState) {
@@ -41,11 +47,20 @@ public class CalendarFragment extends Fragment implements OnDateSelectedListener
         presenter = new CalendarPresenter(this);
         presenter.loadUserCalender("20");
         calendarView = view.findViewById(R.id.events_calendar);
+        calendarView.setOnDateChangedListener(this);
+        calendarDaysWithEventIds = new HashMap<>();
     }
 
     @Override
     public void onDateSelected(@NonNull MaterialCalendarView materialCalendarView, @NonNull CalendarDay calendarDay, boolean b) {
-
+        Log.i("calendarDay = ", calendarDay.toString());
+        if (calendarDays.contains(calendarDay)) {
+//            Toast.makeText(getActivity(), "u did it!", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(getActivity(), EventDetailActivity.class);
+            intent.putExtra("eventId", calendarDaysWithEventIds.get(calendarDay));
+            intent.putExtra("imgUrl", "https://image.flaticon.com/icons/svg/223/223222.svg");
+            startActivity(intent);
+        }
     }
 
     @Override
@@ -60,7 +75,6 @@ public class CalendarFragment extends Fragment implements OnDateSelectedListener
 
     @Override
     public void calendarReady(List<Event> events) {
-        List<CalendarDay> calendarDays = new ArrayList<>();
 
         for (int i = 0; i < events.size(); i++) {
             String[] s = events.get(i).getDate().split("-");
@@ -69,9 +83,9 @@ public class CalendarFragment extends Fragment implements OnDateSelectedListener
             // I can't use JodaTime because MaterialCalendarView only accepts Java Calendar
             int month = Integer.parseInt(s[1]) - 1;
             int day = Integer.parseInt(s[2]);
-//            calendar.set(year, month, day);
             CalendarDay calendarDay = CalendarDay.from(year, month, day);
             calendarDays.add(calendarDay);
+            calendarDaysWithEventIds.put(calendarDay, events.get(i).getId());
         }
         if (calendarDays.size() == events.size()) {
             calendarView.addDecorator(new EventDecorator(Color.BLUE, calendarDays, getActivity()));
