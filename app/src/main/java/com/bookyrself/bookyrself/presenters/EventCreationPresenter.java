f99d8d9e58bec1e505fb8d45179c211f7dbdca11
manbradcalf <ben.medcalf@gmail.com>
2018-09-23 19:13:25 -0400

    EventCreationPresenter in progress

--- app/src/main/java/com/bookyrself/bookyrself/presenters/EventCreationPresenter.java
+++ app/src/main/java/com/bookyrself/bookyrself/presenters/EventCreationPresenter.java
@@ -1,31 +1,30 @@
 package com.bookyrself.bookyrself.presenters;
 
-import android.support.annotation.NonNull;
-
 import com.bookyrself.bookyrself.interactors.ContactsInteractor;
 import com.bookyrself.bookyrself.interactors.EventsInteractor;
-import com.bookyrself.bookyrself.models.SerializedModels.EventDetailResponse.EventDetailResponse;
+import com.bookyrself.bookyrself.interactors.UsersInteractor;
+import com.bookyrself.bookyrself.models.SerializedModels.EventDetail.EventDetail;
+import com.bookyrself.bookyrself.models.SerializedModels.EventDetail.User;
 import com.bookyrself.bookyrself.models.SerializedModels.SearchResponseUsers.Event;
 import com.bookyrself.bookyrself.models.SerializedModels.SearchResponseUsers._source;
-import com.bookyrself.bookyrself.services.FirebaseService;
+import com.bookyrself.bookyrself.models.SerializedModels.User.MiniEvent;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 /**
  * Created by benmedcalf on 3/9/18.
  */
 
-public class EventCreationPresenter implements ContactsInteractor.ContactsInteractorListener, EventsInteractor.EventsInteractorListener {
+public class EventCreationPresenter implements ContactsInteractor.ContactsInteractorListener, EventsInteractor.EventsInteractorListener, UsersInteractor.UsersInteractorListener {
 
     private HashMap<String, Long> userEventCountHashMap;
     private EventCreationPresenterListener presenterListener;
     private ContactsInteractor contactsInteractor;
     private EventsInteractor eventsInteractor;
+    private UsersInteractor usersInteractor;
 
     /**
      * Constructor
@@ -33,36 +32,36 @@ public class EventCreationPresenter implements ContactsInteractor.ContactsIntera
     public EventCreationPresenter(EventCreationPresenterListener listener) {
         this.contactsInteractor = new ContactsInteractor(this);
         this.eventsInteractor = new EventsInteractor(this);
+        this.usersInteractor = new UsersInteractor(this);
         this.presenterListener = listener;
     }
 
     /**
      * Methods
      */
-    public void addToUserEventsCountHashMap(final String userid) {
 
-        mService.getAPI().getUserEvents(userid).enqueue(new Callback<List<Event>>() {
-            @Override
-            public void onResponse(@NonNull Call<List<Event>> call, @NonNull Response<List<Event>> response) {
-                if (response.body() != null) {
-                    userEventCountHashMap.put(userid, (long) response.body().size());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Event>> call, Throwable t) {
-
-            }
-        });
-    }
-
-    public void createEvent(Event event) {
+    //TODO: I may not need this mularkey if I can keep ES from automapping
+//    public void addToUserEventsCountHashMap(final String userid) {
+//
+//        mService.getAPI().getUserEvents(userid).enqueue(new Callback<List<MiniEvent>>() {
+//            @Override
+//            public void onResponse(@NonNull Call<List<MiniEvent>> call, @NonNull Response<List<MiniEvent>> response) {
+//                if (response.body() != null) {
+//                    userEventCountHashMap.put(userid, (long) response.body().size());
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<List<MiniEvent>> call, Throwable t) {
+//
+//            }
+//        });
+//    }
+    public void createEvent(EventDetail event) {
 
         // Iterate through the hashmap of users and their events array size.
         // For each user in the hashmpap, add the event to their
-        for (String userId : userEventCountHashMap.keySet()) {
-            mService.getAPI().addEventToUser(event, userId, userEventCountHashMap.get(userId));
-        }
+        eventsInteractor.createEvent(event);
     }
 
 
@@ -71,9 +70,10 @@ public class EventCreationPresenter implements ContactsInteractor.ContactsIntera
      */
     @Override
     public void contactsReturned(List<String> ids) {
-        for (String id : ids) {
-            addToUserEventsCountHashMap(id);
-        }
+        //TODO: I may not need this mularkey if I can keep ES from automapping
+//        for (String id : ids) {
+//            addToUserEventsCountHashMap(id);
+//        }
     }
 
     @Override
@@ -91,7 +91,7 @@ public class EventCreationPresenter implements ContactsInteractor.ContactsIntera
      */
 
     @Override
-    public void eventDetailReturned(EventDetailResponse event) {
+    public void eventDetailReturned(EventDetail event) {
 
     }
 
@@ -101,15 +101,32 @@ public class EventCreationPresenter implements ContactsInteractor.ContactsIntera
     }
 
     @Override
+    public void eventCreated(String eventId, MiniEvent miniEvent, List<User> usersToInvite) {
+        //        for (User user : usersToInvite) {
+//              //TODO: Fix the value of eventId. Needs to be string across the board
+//            usersInteractor.addEventToUser(miniEvent, user.getUserId(), String.valueOf(miniEvent.getId()));
+//        }
+        ArrayList<String> testList = new ArrayList<>(Arrays.asList("1", "2", "3", "4"));
+        for (String item : testList) {
+            usersInteractor.addEventToUser(miniEvent, item, eventId);
+        }
+    }
+
+    @Override
     public void presentError(String error) {
 
+    }
+
+    @Override
+    public void eventAddedToUserSuccessfully() {
+        //TODO: Find a way to determine it was added to _all_ invited users
     }
 
     /**
      * Contract / Listener
      */
     public interface EventCreationPresenterListener {
-        void createEvent(String[] userIds);
+        void eventCreated();
 
         void addToPotentialUsers(String userId);
 
