97a8cbb92ca413e169ffb0d1c45363d37d22c245
manbradcalf <ben.medcalf@gmail.com>
2019-01-20 17:05:31 -0500

    UsersInteractor updates
    Now it has two interfaces. One for event invites and one for events fragment. Added getUserInvites method

--- app/src/main/java/com/bookyrself/bookyrself/interactors/UsersInteractor.java
+++ app/src/main/java/com/bookyrself/bookyrself/interactors/UsersInteractor.java
@@ -1,10 +1,13 @@
 package com.bookyrself.bookyrself.interactors;
 
+import android.util.Log;
+
 import com.bookyrself.bookyrself.models.SerializedModels.User.EventInfo;
 import com.bookyrself.bookyrself.models.SerializedModels.User.User;
 import com.bookyrself.bookyrself.services.FirebaseService;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -12,13 +15,19 @@ import retrofit2.Response;
 
 public class UsersInteractor {
     private FirebaseService service;
-    private UsersInteractorListener listener;
+    private UsersInteractorListener usersInteractorListener;
+    private UsersEventInvitesInteractorListener usersEventInvitesInteractorListener;
 
     /**
-     * Constructor
+     * Constructors
      */
     public UsersInteractor(UsersInteractorListener listener) {
-        this.listener = listener;
+        this.usersInteractorListener = listener;
+        service = new FirebaseService();
+    }
+
+    public UsersInteractor(UsersEventInvitesInteractorListener listener) {
+        this.usersEventInvitesInteractorListener = listener;
         service = new FirebaseService();
     }
 
@@ -26,7 +35,7 @@ public class UsersInteractor {
         service.getAPI().addEventToUser(eventInfo, userId, eventId).enqueue(new Callback<EventInfo>() {
             @Override
             public void onResponse(Call<EventInfo> call, Response<EventInfo> response) {
-                listener.eventAddedToUserSuccessfully();
+                usersInteractorListener.eventAddedToUserSuccessfully();
             }
 
             @Override
@@ -36,12 +45,35 @@ public class UsersInteractor {
         });
     }
 
+    public void getUserInvites(final String userid) {
+        service.getAPI().getUsersEventInvites(userid).enqueue(new Callback<HashMap<String, EventInfo>>() {
+            @Override
+            public void onResponse(Call<HashMap<String, EventInfo>> call, Response<HashMap<String, EventInfo>> response) {
+                if (response.body() != null) {
+                    for (Map.Entry<String, EventInfo> entry : response.body().entrySet()) {
+                        // If invite is not accepted, tell the listener
+                        if (!entry.getValue().getIsInviteAccepted() && !entry.getValue().getIsHost()) {
+                            usersEventInvitesInteractorListener.eventIdOfEventWithPendingInvitesReturned(entry.getKey());
+                        }
+                    }
+                } else {
+                    Log.e("getUserInvites:", "response is null");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<HashMap<String, EventInfo>> call, Throwable t) {
+                Log.e("getUserInvites:", t.getMessage());
+            }
+        });
+    }
+
     public void getUserDetails(final String userId) {
         service.getAPI().getUserDetails(userId).enqueue(new Callback<User>() {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
                 //TODO: Will the userId here always be the one that made the call?
-                listener.userDetailReturned(response.body(), userId);
+                usersInteractorListener.userDetailReturned(response.body(), userId);
             }
 
             @Override
@@ -55,5 +87,10 @@ public class UsersInteractor {
         void eventAddedToUserSuccessfully();
 
         void userDetailReturned(User user, String userId);
+    }
+
+    public interface UsersEventInvitesInteractorListener {
+        void eventIdOfEventWithPendingInvitesReturned(String eventId);
+
     }
 }
