af86aac2444aa3f147797c33e851db62b328297c
manbradcalf <ben.medcalf@gmail.com>
2019-03-03 13:15:41 -0500

    Set url in user detail

--- app/src/main/java/com/bookyrself/bookyrself/views/UserDetailActivity.java
+++ app/src/main/java/com/bookyrself/bookyrself/views/UserDetailActivity.java
@@ -7,6 +7,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -166,6 +168,14 @@ public class UserDetailActivity extends AppCompatActivity implements UserDetailP
         emailUserTextView.setText(getString(R.string.email_user, user.getUsername()));
         userEmailAddress = user.getEmail();
         emailUserCardview.setOnClickListener(v -> emailUser());
+
+        // Set URL
+        urlTextView.setClickable(true);
+        urlTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        String linkedText =
+                String.format("<a href=\"%s\">%s</a> ", ("http://" + user.getUrl()), user.getUrl());
+        urlTextView.setText(Html.fromHtml(linkedText));
 
         // Determine if this user is a contact
         if (FirebaseAuth.getInstance().getCurrentUser() != null) {
