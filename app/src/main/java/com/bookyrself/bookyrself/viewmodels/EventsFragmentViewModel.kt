0723f377b18e269d7f404173382bf1e3bb220143
manbradcalf <ben.medcalf@gmail.com>
2019-10-25 08:35:33 -1000

    eventsfragmentviewmodel refactor
    using eventsrepo for network calls for eventdata

--- app/src/main/java/com/bookyrself/bookyrself/viewmodels/EventsFragmentViewModel.kt
+++ app/src/main/java/com/bookyrself/bookyrself/viewmodels/EventsFragmentViewModel.kt
@@ -1,64 +1,40 @@
 package com.bookyrself.bookyrself.viewmodels
 
-import android.util.Log
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.bookyrself.bookyrself.data.ServerModels.EventDetail.EventDetail
-import com.bookyrself.bookyrself.services.FirebaseServiceCoroutines
-import com.bookyrself.bookyrself.services.clients.UsersClient
-import com.google.firebase.auth.FirebaseAuth
+import com.bookyrself.bookyrself.data.events.EventsRepository
+import com.bookyrself.bookyrself.data.events.EventsRepositoryResponse.Failure
+import com.bookyrself.bookyrself.data.events.EventsRepositoryResponse.Success
+import com.bookyrself.bookyrself.data.serverModels.EventDetail.EventDetail
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 /**
  * Created by benmedcalf on 3/11/18.
  */
-class EventsFragmentViewModel : BaseViewModel() {
+class EventsFragmentViewModel(application: Application) : BaseViewModel(application, true) {
     var eventDetailsHashMap = MutableLiveData<HashMap<EventDetail, String>>()
-    var signedOutMessage = MutableLiveData<String>()
+    private val eventsRepo = EventsRepository.getInstance(application)
 
-    init {
-        if (FirebaseAuth.getInstance().uid != null) {
-            //TODO: Why do I have to !! here if i'm null checking above
-            loadUsersEventInfo(userId!!)
-        } else {
-            signedOutMessage.value = "Sign in to see your events!"
-        }
-    }
-
-    //TODO: Copied over from UserDetailViewModel. This will also be needed in ProfileViewModel. How to consolidte considering they rely on livedata in the activity?
-    private fun loadUsersEventInfo(userId: String) {
-        //TODO: Check if the cache is dirty here. If it is we need to go to network
-        val events = HashMap<EventDetail, String>()
-
+    override fun load() {
         CoroutineScope(Dispatchers.IO).launch {
-            val userResponse = UsersClient.service.getUserDetails(userId)
-            if (userResponse.isSuccessful && userResponse.body()?.events?.keys != null) {
-                userResponse.body()?.events?.keys?.forEach { eventId ->
-                    val eventDetailResponse = service.getEventData(eventId)
-                    withContext(Dispatchers.Main) {
-                        if (eventDetailResponse.isSuccessful && eventDetailResponse.body() != null) {
-                            events[eventDetailResponse.body()!!] = eventId
-                            eventDetailsHashMap.value = events
-                        } else if (eventDetailResponse.isSuccessful && eventDetailResponse.body() == null) {
-                            Log.e("EventsFragmentViewModel", "No data for eventId $eventId")
-                        } else {
-                            errorMessage.value = eventDetailResponse.message()
-                        }
-                    }
+            when (val response = eventsRepo.getAllEvents(userId!!)) {
+                is Success -> {
+                    eventDetailsHashMap.value = response.events
+                }
+                is Failure -> {
+                    errorMessage.value = response.errorMessage
                 }
             }
         }
     }
 
-    //TODO: Genericize this?
-    class EventsFragmentViewModelFactory() : ViewModelProvider.Factory {
-
+    class EventsFragmentViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return EventsFragmentViewModel() as T
+            return EventsFragmentViewModel(application) as T
         }
     }
 }
