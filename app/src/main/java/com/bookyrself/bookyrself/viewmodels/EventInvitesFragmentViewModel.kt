d6c018f51ea59caf2ecbb061a29fab50ec00049f
manbradcalf <ben.medcalf@gmail.com>
2019-10-25 08:35:06 -1000

    eventinvitesfragmentviewmodel refactor
    all the network logic now lives in the eventsrepo

--- app/src/main/java/com/bookyrself/bookyrself/viewmodels/EventInvitesFragmentViewModel.kt
+++ app/src/main/java/com/bookyrself/bookyrself/viewmodels/EventInvitesFragmentViewModel.kt
@@ -1,94 +1,51 @@
 package com.bookyrself.bookyrself.viewmodels
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
-import com.bookyrself.bookyrself.data.ServerModels.EventDetail.EventDetail
-import com.bookyrself.bookyrself.data.ServerModels.User.EventInviteInfo
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.bookyrself.bookyrself.data.events.EventsRepository
+import com.bookyrself.bookyrself.data.events.EventsRepositoryResponse.*
+import com.bookyrself.bookyrself.data.serverModels.EventDetail.EventDetail
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import java.util.*
 
-class EventInvitesFragmentViewModel : BaseViewModel() {
-    var eventsWithPendingInvites = MutableLiveData<HashMap<EventDetail, String>>()
-    var noEventsWithPendingInvitesReturned = MutableLiveData<Boolean>()
+class EventInvitesFragmentViewModel(application: Application) : BaseViewModel(application, true) {
+    val eventsWithPendingInvites = MutableLiveData<HashMap<EventDetail, String>>()
 
-    // TODO: I really hate having to manage all these different hashmaps and lists
-    private val eventIdsOfPendingEvents = mutableListOf<String>()
-    private val eventsOfPendingInvitesHashMap = HashMap<EventDetail, String>()
+    private val eventsRepo = EventsRepository.getInstance(application)
 
     override fun load() {
         CoroutineScope(Dispatchers.IO).launch {
-            val eventInvitesResponse =
-                    service.getUsersEventInvites(userId!!)
-
-            //TODO: Look at the logic here. I want to notify view if we make a call but there are no pending invites
-            if (eventInvitesResponse.isSuccessful) {
-                // Get all the event invites and grab only pending invites
-                withContext(Dispatchers.Main) {
-                    eventInvitesResponse.body()?.forEach { entry ->
-                        if (isInvitePendingResponse(entry)) {
-                            eventIdsOfPendingEvents.add(entry.key)
-                        }
-                    }
-                    // Now get all the pending invite details
-                    for (eventId in eventIdsOfPendingEvents) {
-                        val eventWithPendingInviteResponse =
-                                service.getEventData(eventId)
-                        if (eventWithPendingInviteResponse.body() != null) {
-                            // update function local EventDetail, EventId hashmap
-                            eventsOfPendingInvitesHashMap[eventWithPendingInviteResponse.body()!!] = eventId
-                            // set LiveData value to local hashmap
-                            eventsWithPendingInvites.value = eventsOfPendingInvitesHashMap
-                        }
-                    }
+            when (val response = eventsRepo.getEventsWithPendingInvites(userId!!)) {
+                is Success -> {
+                    eventsWithPendingInvites.value = response.events
+                }
+                is Failure -> {
+                    errorMessage.value = response.errorMessage
                 }
             }
         }
     }
 
     fun respondToInvite(accepted: Boolean, eventId: String, eventDetail: EventDetail) {
-        when (accepted) {
-            true -> {
-                CoroutineScope(Dispatchers.IO).launch {
-                    val acceptInviteResponse = service.acceptInvite(true, userId!!, eventId)
-                    if (acceptInviteResponse.isSuccessful) {
-                        val updateEventDetailWithUserResponse =
-                                service.setEventUserAsAttending(true, userId, eventId)
-                        if (updateEventDetailWithUserResponse.isSuccessful) {
-                            withContext(Dispatchers.Main) {
-                                // TODO: I really hate having to manage all these different hashmaps and lists
-                                eventsOfPendingInvitesHashMap.remove(eventDetail)
-                                eventsWithPendingInvites.value = eventsOfPendingInvitesHashMap
-                            }
-                        } else {
-                            // TODO: Failed to update userId node on event object
-                        }
-                    } else {
-                        // TODO: Failed to update invite node on user object
-                    }
+        CoroutineScope(Dispatchers.IO).launch {
+            when (val response = eventsRepo.respondToInvite(accepted, userId!!, eventId, eventDetail)) {
+                is Success -> {
+                    eventsWithPendingInvites.value = response.events
                 }
-            }
-            false -> {
-                CoroutineScope(Dispatchers.IO).launch {
-                    val rejectInviteResponse = service.rejectInvite(true, userId!!, eventId)
-                    if (rejectInviteResponse.isSuccessful) {
-                        withContext(Dispatchers.Main) {
-                            // TODO: I really hate having to manage all these different hashmaps and lists
-                            eventsOfPendingInvitesHashMap.remove(eventDetail)
-                            eventsWithPendingInvites.value = eventsOfPendingInvitesHashMap
-                        }
-                    }
+                is Failure -> {
+                    errorMessage.value = response.errorMessage
                 }
             }
         }
     }
 
-    private fun isInvitePendingResponse(eventInvite: Map.Entry<String, EventInviteInfo>?): Boolean {
-        // All fields are false by default, so if all fields remain false,
-        // because every interaction flips a flag to true,
-        // it means the user hasn't interacted at all with the invite so the invite is pending
-        return ((!eventInvite?.value?.isInviteAccepted!!)
-                && (!eventInvite.value.isInviteRejected!!)
-                && (!eventInvite.value.isHost!!))
+    class EventInvitesFragmentViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return EventInvitesFragmentViewModel(application) as T
+        }
     }
 }
