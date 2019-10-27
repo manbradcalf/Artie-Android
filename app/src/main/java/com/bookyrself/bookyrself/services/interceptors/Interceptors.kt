6fb166a4869e2576fb7d7791e1bf36d7eb7a0459
manbradcalf <ben.medcalf@gmail.com>
2019-10-27 16:31:41 -0400

    random interceptor refactoring - for debugging - WIP

--- app/src/main/java/com/bookyrself/bookyrself/services/interceptors/Interceptors.kt
+++ app/src/main/java/com/bookyrself/bookyrself/services/interceptors/Interceptors.kt
@@ -1,11 +1,17 @@
 package com.bookyrself.bookyrself.services.interceptors
 
+import com.bookyrself.bookyrself.data.serverModels.SearchResponseEvents.User
+import com.google.firebase.auth.FirebaseAuth
 import okhttp3.Interceptor
 import okhttp3.Protocol
+import okhttp3.Response
+import okhttp3.ResponseBody
+import java.io.ByteArrayInputStream
+import kotlin.random.Random
 
-class FailUserDetailInterceptor : Interceptor {
+class MockSearchResultsInterceptor : Interceptor {
 
-    override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+    override fun intercept(chain: Interceptor.Chain): Response {
         return if (chain.request().url().pathSegments().contains("users")) {
             chain.proceed(chain.request())
                     .newBuilder()
