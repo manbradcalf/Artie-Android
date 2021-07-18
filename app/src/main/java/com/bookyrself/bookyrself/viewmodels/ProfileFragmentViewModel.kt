4f0fd56840260ce1b313dcfd3eb5cd5bccdfa604
manbradcalf <ben.medcalf@gmail.com>
2021-07-18 12:59:01 -0400

    add VMFactory for ProfileFragmentViewModel

--- app/src/main/java/com/bookyrself/bookyrself/viewmodels/ProfileFragmentViewModel.kt
+++ app/src/main/java/com/bookyrself/bookyrself/viewmodels/ProfileFragmentViewModel.kt
@@ -2,15 +2,17 @@ package com.bookyrself.bookyrself.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.bookyrself.bookyrself.data.profile.ProfileRepo
+import com.bookyrself.bookyrself.data.profile.ProfileRepoResponse.Failure
+import com.bookyrself.bookyrself.data.profile.ProfileRepoResponse.Success
 import com.bookyrself.bookyrself.data.serverModels.EventDetail.EventDetail
 import com.bookyrself.bookyrself.data.serverModels.User.User
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import com.bookyrself.bookyrself.data.profile.ProfileRepoResponse.Success
-import com.bookyrself.bookyrself.data.profile.ProfileRepoResponse.Failure
 
 
 class ProfileFragmentViewModel(application: Application) : BaseViewModel(application) {
@@ -30,6 +32,13 @@ class ProfileFragmentViewModel(application: Application) : BaseViewModel(applica
                     }
                 }
             }
+        }
+    }
+
+    class ProfileFragmentViewModelFactory(private val application: Application) :
+        ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            return ProfileFragmentViewModel(application) as T
         }
     }
 }
